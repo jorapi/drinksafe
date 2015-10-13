016b597dd2eff27ec80b71eab5e26cc4ea2dcb88
Bmgaynor <bmgaynor@uwm.edu>
2015-10-13 13:02:25 -0500

    removed the root.js route in favor of static client side serve up

--- project/server/boot/root.js
+++ project/server/boot/root.js
@@ -1,6 +1,11 @@
-module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
-};
+// module.exports = function(server) {
+//   // Install a `/` route that returns server status
+//   var router = server.loopback.Router();
+//   //router.get('/', server.loopback.status());
+//   router.get('/',function(req, res){
+//     res.json({
+//       hello: 'world'
+//     });
+//   });
+//   server.use(router);
+// };
