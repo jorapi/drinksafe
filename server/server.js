7d457af7fb24dc3409cf4904cfd1fde4495fca67
jorapi <jorapi@gmail.com>
2015-10-25 20:44:40 -0500

    Facebook integration finished. Theoretically should be able to log in using Facebook.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,22 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
 var app = module.exports = loopback();
+
+// Create an instance of PassportConfigurator with the app instance
+var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+// Load the provider configurations
+var config = {};
+try {
+ config = require('./providers.json');
+} catch(err) {
+ console.error('Please configure your passport strategy in `providers.json`.');
+ console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
+ process.exit(1);
+}
+// Initialize passport
+passportConfigurator.init();
 
 app.start = function() {
   // start the web server
@@ -25,3 +40,16 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+// Set up related models
+passportConfigurator.setupModels({
+ userModel: app.models.user,
+ userIdentityModel: app.models.userIdentity,
+ userCredentialModel: app.models.userCredential
+});
+// Configure passport strategies for third party auth providers
+for(var s in config) {
+ var c = config[s];
+ c.session = c.session !== false;
+ passportConfigurator.configureProvider(s, c);
+}
