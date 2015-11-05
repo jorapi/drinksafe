94e07c5941c2449d00c554785cd43c243afbfd89
Brad Gaynor <bmgaynor@uwm.edu>
2015-11-04 20:51:23 -0600

    Fucking pushin

--- common/models/drink.js
+++ common/models/drink.js
@@ -1,3 +1,23 @@
 module.exports = function(Drink) {
+  Drink.add = function(drink, ingredients, cb) {
+      console.log('this shit');
+      cb(null, drink, ingredients);
 
+  };
+  Drink.remoteMethod(
+    'add', {
+      http: {
+        path: '/add',
+        verb: 'post'
+      },
+      accepts:[
+        {arg: 'drink', type: 'string'},
+        {arg: 'ingredients', type: 'string'}
+      ],
+      returns: {
+        drink: 'string',
+        type: 'string'
+      }
+    }
+  );
 };
