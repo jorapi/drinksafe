ee9b0fdf04b7dd3112cf82927c08f4f516c01a3f
jorapi <jorapi@gmail.com>
2015-11-18 17:28:22 -0600

    Hey Nate, go comment on a sweet drink

--- src/app/commentsBox/controller.js
+++ src/app/commentsBox/controller.js
@@ -1,5 +1,5 @@
 angular.module('RDash')
   .controller('commentsBoxController', function($scope, $http, $location) {
       //below needs to change to the current url
-      $scope.commentURL = "http://drinksafe.herokuapp.com" + $location.path();
+      $scope.commentURL = $location.absUrl();
   });
