3b6b8a58fd6479fb09c2f09f44a6b167bd3ea18d
jorapi <jorapi@gmail.com>
2015-11-18 15:41:10 -0600

    Liquor cabinet implemented

--- src/app/userCabinet/controller.js
+++ src/app/userCabinet/controller.js
@@ -8,9 +8,59 @@ angular.module('RDash')
         if (response.profiles != null && response.profiles.length > 0){
           $scope.hideUserMenu = false;
         }
-        $http.get("api/drinks")
-          .success(function(data) {
-            $scope.userIngredients = data;
-        });
       });
+
+      $scope.ingredients = [];
+
+      $http.get("/api/ingredients")
+        .success(function(response) {
+          $scope.dataIngredients = response;
+        });
+
+      $scope.currIng = {};
+
+      $http.get("/api/users/me/")
+        .success(function(response) {
+          $scope.me = response;
+          for (i = 0; i < $scope.me._amounts.length; ++i) {
+            $scope.currentibecauseangularisdumb = i;
+            $http.get("/api/ingredients/5647bd23141014a838417b7c")
+              .success(function(response) {
+                $scope.currIng = response;
+                $scope.ingredients.push({
+                  "text": $scope.currIng.text,
+                  "type": $scope.currIng.type,
+                  "amount": $scope.me._amounts[$scope.currentibecauseangularisdumb].amount,
+                  "unit": $scope.me._amounts[$scope.currentibecauseangularisdumb].unit,
+                  "id": $scope.currIng.id
+                });
+              });
+          }
+        });
+
+      $scope.addIng = function(item) {
+        var index = $scope.dataIngredients.indexOf(item);
+        var ing = $scope.dataIngredients[index];
+        $scope.ingredients.push(ing);
+        $scope.dataIngredients.splice(index, 1);
+        $scope.me._amounts.push({
+          "ingredientID": ing.id,
+          "amount": ing.amount,
+          "unit": "oz",
+          "id": $scope.me._amounts.length + 1
+        });
+        $http.put("/api/users/me", $scope.me);
+      };
+
+      $scope.remove = function(item) {
+        var index = $scope.ingredients.indexOf(item);
+        $scope.dataIngredients.push($scope.ingredients[index]);
+        for (i = 0; i < $scope.me._amounts.length; ++i) {
+          if(item.id == $scope.me._amounts[i].ingredientID){
+            $scope.me._amounts.splice(i, 1);
+          }
+        }
+        $http.put("/api/users/me", $scope.me);
+        $scope.ingredients.splice(index, 1);
+      };
   });
