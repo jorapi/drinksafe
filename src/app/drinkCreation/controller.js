8883a26b00a4ce37c7c60d952c3955cc64efe130
njmaerz <nathanmaerz@gmail.com>
2015-11-22 15:59:11 -0600

    Ingredient Creation

--- src/app/drinkCreation/controller.js
+++ src/app/drinkCreation/controller.js
@@ -3,9 +3,9 @@ angular.module('RDash')
     //check if user is logged in
     $scope.hideUserMenu = false;
     $http.get("/auth/user")
-     .success(function(response){
-       $scope.userJSON = response;
-        if (response.profiles != null && response.profiles.length > 0){
+      .success(function(response) {
+        $scope.userJSON = response;
+        if (response.profiles != null && response.profiles.length > 0) {
           $scope.hideUserMenu = false;
         }
       });
@@ -29,11 +29,18 @@ angular.module('RDash')
     $scope.image = {
       "file": "",
       "id": ""
-    }
+    };
+    $scope.add = {
+      "text": "",
+      "type": ""
+    };
     $scope.directionStep;
+    $scope.newIngredient = [];
     $scope.directions = [];
     $scope.ingredients = [];
     $scope.drikPost = false;
+    $scope.validType = true;
+    $scope.validText = true;
     $http.get("/api/ingredients")
       .success(function(response) {
         $scope.dataIngredients = response;
@@ -45,7 +52,7 @@ angular.module('RDash')
       for (i = 0; i < $scope.directions.length; ++i) {
         holdDirections.push($scope.directions[i].text);
       }
-      angular.forEach($scope.ingredients, function(ing, i){
+      angular.forEach($scope.ingredients, function(ing, i) {
         $scope.drink._amounts.push({
           "ingredientID": ing.id,
           "amount": ing.amount,
@@ -54,7 +61,7 @@ angular.module('RDash')
         });
       });
 
-      $scope.drink.photo=$scope.currentImage;
+      $scope.drink.photo = $scope.currentImage;
       $scope.drink.instructions = holdDirections;
       console.log($scope.drink);
       $http.put("/api/drinks", $scope.drink)
@@ -68,14 +75,21 @@ angular.module('RDash')
 
     $scope.remove = function(item) {
       var index = $scope.ingredients.indexOf(item);
-      $scope.dataIngredients.push($scope.ingredients[index]);
-      $scope.ingredients.splice(index, 1);
+      if ($scope.newIngredient.indexOf(item) === -1) {
+        $scope.dataIngredients.push($scope.ingredients[index]);
+        $scope.ingredients.splice(index, 1);
+      } else {
+        $http.delete("/api/ingredients/" + item.id)
+          .success(function(response) {
+            $scope.ingredients.splice(index, 1);
+          });
+      }
     };
 
     $scope.addIng = function(item) {
       var index = $scope.dataIngredients.indexOf(item);
       var ing = $scope.dataIngredients[index];
-      ing.amount = 0;
+      //ing.amount = 0;
       $scope.ingredients.push(ing);
       $scope.dataIngredients.splice(index, 1);
     };
@@ -101,5 +115,35 @@ angular.module('RDash')
 
     $scope.onSuccess = function(response) {
       $scope.currentImage = response.data.result.url;
+    };
+
+    $scope.addIngredient = function() {
+      var hold = {
+        "text": $scope.add.text,
+        "type": $scope.add.type
+      }
+      if (hold.text.length >= 3 && hold.type.length >= 3) {
+        $scope.add.text = "";
+        $scope.add.type = "";
+        $scope.validType = true;
+        $scope.validText = true;
+        $scope.adding = false;
+        $http.put("/api/ingredients", hold)
+          .success(function(response) {
+            $scope.ingredients.push(response);
+            $scope.newIngredient.push(response);
+          });
+      } else {
+        if (hold.text.length < 3) {
+          $scope.validText = false;
+        } else {
+          $scope.validText = true;
+        }
+        if (hold.type.length < 3) {
+          $scope.validType = false;
+        } else {
+          $scope.validText = true;
+        }
+      }
     };
   });
