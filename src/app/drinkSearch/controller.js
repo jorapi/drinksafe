c06338ed09a47ceb0c5cf6d4777c679a1dd306a2
njmaerz <nathanmaerz@gmail.com>
2015-12-02 15:45:02 -0600

    searching by drink is functionally done

--- src/app/drinkSearch/controller.js
+++ src/app/drinkSearch/controller.js
@@ -40,14 +40,8 @@ angular.module('RDash')
 
     $scope.filterFunction = function(element){
       if($scope.ingredients.length>0){
-        var hold=[];
-        for(var i = 0; i < element._amounts.length; ++i){
-          hold.push(element._amounts[i].ingredientID);
-        }
-        for(var i = 0; i < $scope.ingredients.length; ++i){
-          if(hold.indexOf($scope.ingredients[i])===-1){
-            return false;
-          }
+        if(!filterByIngredient(element)){
+          return false;
         }
       }
       if($scope.search.length<=0){
@@ -60,8 +54,31 @@ angular.module('RDash')
       }
     };
 
-    $scope.addIng = function(id){
-      $scope.ingredients.push(id);
+function filterByIngredient(element){
+  var hold=[];
+  for(var i = 0; i < element._amounts.length; ++i){
+    hold.push(element._amounts[i].ingredientID);
+  }
+  for(var i = 0; i < $scope.ingredients.length; ++i){
+    if(hold.indexOf($scope.ingredients[i].id)===-1){
+      return false;
+    }
+  }
+  return true;
+};
+
+    $scope.addIng = function(item){
+      var index = $scope.dataIngredients.indexOf(item);
+      var ing = $scope.dataIngredients[index];
+      $scope.ingredients.push(ing);
+      $scope.dataIngredients.splice(index, 1);
+    };
+
+    $scope.removeIng = function(item){
+      var index = $scope.ingredients.indexOf(item);
+      var ing = $scope.ingredients[index];
+      $scope.dataIngredients.push(ing);
+      $scope.ingredients.splice(index, 1);
     };
 
     $scope.unexpand=function() {
