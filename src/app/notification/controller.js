12391c1e0d3b0e26f36375e2fa002bd67f6ee335
jorapi <jorapi@gmail.com>
2015-12-09 15:54:58 -0600

    Notification badges bitches

--- src/app/notification/controller.js
+++ src/app/notification/controller.js
@@ -2,12 +2,21 @@
 angular.module('RDash')
 .controller('notificationController', function($scope, $http){
   $scope.isUserLoggedIn = false;
+  $scope.notifications = [];
   $http.get("/auth/user")
     .success(function(response) {
       $scope.userJSON = response;
       if (response.profiles != null && response.profiles.length > 0) {
         $scope.isUserLoggedIn = true;
-
+        $scope.currUser = response;
+        for(i = 0; i < response._amounts.length; i++){
+          if(response._amounts[i].amount < 3){
+            $http.get("/api/ingredients/" + response._amounts[i].ingredientID)
+              .success(function(response2) {
+                $scope.notifications.push("Low on " + response2.text);
+              });
+          }
+        }
       }
     });
 });
