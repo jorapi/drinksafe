40c12249ae0a0a96b61c4a1a421525ad057dd062
njmaerz <nathanmaerz@gmail.com>
2015-12-12 16:39:11 -0600

    Stop search bar from taking 2 lines on mobile

--- src/app/drinkBar/controller.js
+++ src/app/drinkBar/controller.js
@@ -1,11 +1,26 @@
 angular.module('RDash')
-  .controller('drinkBarController', function($scope, $http, $location) {
+  .controller('drinkBarController', function($scope, $http, $location, $window) {
 
 
     $http.get("api/drinks")
       .success(function(data) {
         $scope.drinks = data;
       });
+
+      if($window.innerWidth < 460){
+        $scope.isMobile=true;
+      }
+
+      $scope.$watch(function(){
+       return $window.innerWidth;
+    }, function(value) {
+      if(value < 460){
+        $scope.isMobile=true;
+      }else{
+        $scope.isMobile=false;
+      }
+   });
+
     $scope.selectedDrink = function(selected) {
       if (selected) {
         $location.path("/drinkView").search({drinkId: selected.originalObject.id});
